5529c2fd47ff2da73e0f6dbee5c608c182fcca69
CAD 2 Computer <wtrprogrammer@gmail.com>
2020-01-26 15:43:19 -0800

    l

--- src/main/java/frc5124/robot2020/commands/LocationUpdaterCommand.java
+++ src/main/java/frc5124/robot2020/commands/LocationUpdaterCommand.java
@@ -7,12 +7,15 @@
 
 package frc5124.robot2020.commands;
 
+import java.util.Set;
+
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc5124.robot2020.subsystems.*;
 
-public class LocationUpdaterCommand extends CommandBase {
+public class LocationUpdaterCommand implements Command {
   /**
    * Creates a new LocationUpdaterCommand.
    */
@@ -21,7 +24,8 @@ public class LocationUpdaterCommand extends CommandBase {
   NetworkTableEntry ySlider;
   ComplexWidget rotation;
 
-  public LocationUpdaterCommand(DriveTrain driveTrain, NetworkTableEntry xSlider, NetworkTableEntry ySlider,ComplexWidget rotation) {
+  public LocationUpdaterCommand(DriveTrain driveTrain, NetworkTableEntry xSlider, NetworkTableEntry ySlider,
+      ComplexWidget rotation) {
     this.driveTrain = driveTrain;
     this.xSlider = xSlider;
     this.ySlider = ySlider;
@@ -30,26 +34,24 @@ public class LocationUpdaterCommand extends CommandBase {
   }
 
   // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
 
   // Called every time the scheduler runs while the command is scheduled.
-  @Override
   public void execute() {
     xSlider.setDouble(driveTrain.getLocation().getTranslation().getY());
     ySlider.setDouble(driveTrain.getLocation().getTranslation().getY());
     
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
+  public boolean runsWhenDisabled() {
+    return true;
   }
 
-  // Returns true when the command should end.
   @Override
-  public boolean isFinished() {
-    return false;
+  public Set<Subsystem> getRequirements() {
+    // TODO Auto-generated method stub
+    return Set.of();
   }
 }
--- src/main/java/frc5124/robot2020/commands/UltraSonicSensor.java
+++ src/main/java/frc5124/robot2020/commands/UltraSonicSensor.java
@@ -7,12 +7,15 @@
 
 package frc5124.robot2020.commands;
 
+import java.util.Set;
+
 import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc5124.robot2020.subsystems.Loader;
 
 
-public class UltraSonicSensor extends CommandBase {
+public class UltraSonicSensor implements Command {
   /**
    * Creates a new UltraSonicSensor.
    */
@@ -26,27 +29,20 @@ public class UltraSonicSensor extends CommandBase {
   }
 
   // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
 
   // Called every time the scheduler runs while the command is scheduled.
-  @Override
   public void execute() {
       ultraSonic.setDouble(loader.currentDistance);
       if(loader.currentDistance < 10){
         ultraSonic.setBoolean(true);
       }
   }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
+  public Set<Subsystem> getRequirements(){
+    return Set.of();
   }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+  @Override 
+  public boolean runsWhenDisabled(){
+    return true;
   }
 }
