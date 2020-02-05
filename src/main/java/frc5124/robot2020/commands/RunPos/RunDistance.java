c8e558d22f22657e9b64ba5b26ff7f049fbe7b05
IGotYouMrT <57026847+IGotYouMrT@users.noreply.github.com>
2020-02-04 16:48:40 -0800

    Drivedistance

--- src/main/java/frc5124/robot2020/commands/RunPos/RunDistance.java
+++ src/main/java/frc5124/robot2020/commands/RunPos/RunDistance.java
@@ -10,26 +10,70 @@ package frc5124.robot2020.commands.RunPos;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc5124.robot2020.subsystems.DriveTrain;
 
-public class RunDistance extends CommandBase {
-  private DriveTrain driveTrain;
-  
-  /**
-   * Creates a new RunDistance.
-   */
-  public RunDistance() {
-    driveTrain = new DriveTrain();
-    addRequirements(driveTrain);
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
+import javax.swing.GroupLayout.ParallelGroup;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+
+public class RunToPosition extends CommandBase {
+private Pose2d currentPos;
+private double targetTheta;
+private DriveTrain driveTrain;
+private double transX;
+private double transY;
+private double targetDistance;
+private ParallelCommandGroup runPos = new ParallelCommandGroup();
+private double currentDistance;
+private PIDController distanceController = new PIDController(0, 0, 0);
+private PIDController angleController = new PIDController(0,0,0); 
+
+
+
+  /**
+   * Creates a new RunToPosition.
+   */
+   /**
+   * 
+   * Creates a new RunToPosition.
+   * @param transX translation in X axis
+   * @param transY translation in Y axis
+   * 
+   */
+  public RunToPosition(DriveTrain subsystem, double transX, double transY) {
+    driveTrain = subsystem;
+    addRequirements(driveTrain);
+    this.transX = transX;
+    this.transY = transY;
+      // Use addRequirements() here to declare subsystem dependencies.
+  }
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-  }
+  
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+        currentPos =  driveTrain.getLocation();
+        targetTheta = Math.atan((transX/transY));
+        double turn = angleController.calculate(driveTrain.getGryoDegree(), targetTheta);
+
+        if(driveTrain.getGryoDegree() <= targetTheta){
+          driveTrain.arcadeDrive(0,turn);
+        }
+        else{
+          targetDistance = Math.sqrt((transX*transX)+(transY*transY));
+          double currentX = (currentPos.getTranslation().getX() + transX);
+          double currentY = (currentPos.getTranslation().getY() + transY); 
+        
+          currentDistance =  Math.sqrt((currentX * currentX)+(currentY * currentY));
+
+          distanceController.calculate(currentDistance, targetDistance);
+        }
+        
 
 
   }
@@ -45,3 +89,4 @@ public class RunDistance extends CommandBase {
     return false;
   }
 }
+
