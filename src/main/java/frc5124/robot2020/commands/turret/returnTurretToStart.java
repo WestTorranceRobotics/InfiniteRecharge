22a8f5620c2c85a661915309517874e591b906e5
CAD 2 Computer <wtrprogrammer@gmail.com>
2020-02-08 17:44:16 -0800

    Added basic returnTurretToStart command for turret, added limit for rotating turret, fixed capitalization for one command

---
+++ src/main/java/frc5124/robot2020/commands/turret/returnTurretToStart.java
@@ -0,0 +1,67 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc5124.robot2020.commands.turret;
+
+import com.revrobotics.CANPIDController;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc5124.robot2020.subsystems.Turret;
+
+public class returnTurretToStart extends CommandBase {
+  private Turret turret;
+  private CANPIDController turretPID;
+  private boolean isDone = false;
+
+  /**
+   * Creates a new returnTurretToStart.
+   */
+  public returnTurretToStart(Turret subsystem) {
+    turret = subsystem;
+    addRequirements(turret);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    turretPID = turret.getMotor().getPIDController();
+    turretPID.setP(.0004);
+    turretPID.setI(0);
+    turretPID.setD(0);
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(turret.getEncoderCountsPerRevolution() < 0){
+      turret.getMotor().set(0.3);
+    }
+    if(turret.getEncoderCountsPerRevolution() > 0){
+      turret.getMotor().set(-0.3);
+    }
+    else{
+      isDone = true;
+    }
+    
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    //sets turret motor and encoder to 0
+    turret.getMotor().set(0);
+    turret.getEncoder().setPosition(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return isDone;
+  }
+}
--- src/main/java/frc5124/robot2020/subsystems/Turret.java
+++ src/main/java/frc5124/robot2020/subsystems/Turret.java
@@ -6,41 +6,79 @@
 /*----------------------------------------------------------------------------*/
 
 package frc5124.robot2020.subsystems;
+
 import frc5124.robot2020.RobotMap;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class Turret implements Subsystem {
-  public boolean limitReached = false;
   private CANSparkMax turretMotor;
-  //private VictorSP turretMotor;
+  private CANEncoder turretEncoder;
+
+  private double initialValue;
+  private double clockwiseLimit;
+  private double counterClockwiseLimit;
   
   public Turret() {
     turretMotor = new CANSparkMax(RobotMap.TurretMap.turretCanID, MotorType.kBrushless);
-   //turretMotor = new VictorSP(1);
+    turretEncoder = turretMotor.getEncoder();
+    clockwiseLimit = 33.0;
+    counterClockwiseLimit = -clockwiseLimit;
+
+  }
+
+  @Override
+  public void periodic() {
+    //Starting value is 25
+    // +- 33 encoder values
+
+    SmartDashboard.putNumber("Turret Encoder Value", turretEncoder.getPosition());
+    SmartDashboard.updateValues();
   }
 
   public void rotateTurret(double power) {
-    if (limitReached && turretMotor.getAppliedOutput() == 0) {
-      
-    } else if (limitReached) {
+    //if clockwise limit reached, and an attempt is made to keep going clockwise, stop the motor
+    if (turretMotor.getAppliedOutput() == 0 && turretEncoder.getPosition() >= clockwiseLimit && power > 0) {
       turretMotor.set(0);
-      
-    } else if (!limitReached && turretMotor.getAppliedOutput() != power) {
+    } 
+    //if clockwise limit reached, but an attempt is made to go counter-clockwise, move the motor
+    else if (turretMotor.getAppliedOutput() == 0 && turretEncoder.getPosition() >= clockwiseLimit && power < 0) {
       turretMotor.set(power);
-      
+    } 
+
+    //if counter-clockwise limit reached, and an attempt is made to keep going counter-clockwise, stop the motor
+    else if (turretMotor.getAppliedOutput() == 0 && turretEncoder.getPosition() <= counterClockwiseLimit && power < 0) {
+      turretMotor.set(0);
+    } 
+    //if counter-clockwise limit reached, and an attempt is made to go clockwise, move the motor
+    else if (turretMotor.getAppliedOutput() == 0 && turretEncoder.getPosition() >= counterClockwiseLimit && power > 0) {
+      turretMotor.set(power);
     }
 
+    else {
+      turretMotor.set(0);
+    }
   }
 
-  public int getEncoder(){
+  public int getEncoderCountsPerRevolution(){
     return turretMotor.getEncoder().getCountsPerRevolution();
+  }
+
+  public CANSparkMax getMotor() {
+    return turretMotor;
+  }
+
+  public CANEncoder getEncoder(){
+    return turretEncoder;
   }
 
   private boolean limitReached() {
     return true;
   }
-  
-}
+
+} 
