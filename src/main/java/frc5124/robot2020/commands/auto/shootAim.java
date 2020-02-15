717b477fe9ae18976a4814be7f85ffec1bb5384c
unknown <kamranhaq@protonmail.com>
2020-02-14 20:37:23 -0800

    practiceCodeMeet

---
+++ src/main/java/frc5124/robot2020/commands/auto/shootAim.java
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc5124.robot2020.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class shootAim extends ParallelDeadlineGroup {
+  /**
+   * Creates a new shootAim.
+   */
+  public shootAim() {
+    // Add your commands in the super() call.  Add the deadline first.
+    super(new InstantCommand());
+  }
+}
