8848b268bc929ddb8636f5c55ad2c1e4d1f24edd
JaSON A <34526187+GuyWhoCode@users.noreply.github.com>
2020-02-14 19:04:29 -0800

    add 1 line of limit

--- src/main/java/frc5124/robot2020/subsystems/Turret.java
+++ src/main/java/frc5124/robot2020/subsystems/Turret.java
@@ -9,63 +9,116 @@ package frc5124.robot2020.subsystems;
 
 import frc5124.robot2020.RobotMap;
 
-import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
+import com.revrobotics.EncoderType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class Turret implements Subsystem {
   private CANSparkMax turretMotor;
-  public CANPIDController turretPID;
-  private CANEncoder turretEncoder;
-  public double startPosition;
-  public boolean limitReached = false;
-
+  private CANPIDController turretPID;
+  private double startDegrees = 0;
+  public Boolean limitReached = false;
+  // private DigitalInput magneticSensor;
+  // private DigitalOutput mDigitalOutput;
+  
   public Turret() {
     turretMotor = new CANSparkMax(RobotMap.TurretMap.turretCanID, MotorType.kBrushless);
-    turretMotor.setIdleMode(IdleMode.kBrake);
     turretPID = turretMotor.getPIDController();
-    turretEncoder = turretMotor.getEncoder();
     turretMotor.restoreFactoryDefaults();
+    setCoast();
     turretPID.setP(RobotMap.TurretMap.Kp);
-    turretPID.setReference(0, ControlType.kVelocity);
-    startPosition = getEncoder();
+    turretPID.setI(RobotMap.TurretMap.Ki);
+    turretPID.setIZone(RobotMap.TurretMap.KiZone);
+    turretPID.setReference(0, ControlType.kPosition);
+    startDegrees = getDegrees();
+    turretMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, (float) 31.5);
+    turretMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, (float) 31.5);
+    //Code used for PID Tuning
+    // SmartDashboard.putNumber("P", .04);
+    // SmartDashboard.putNumber("I", 0);
+    // SmartDashboard.putNumber("D", 0);
+    // SmartDashboard.putNumber("IZONE", 0);
   }
 
+  //Code used for PID Tuning
+  // public void updateCoeffs() {
+  //   turretPID.setP(SmartDashboard.getNumber("P", RobotMap.TurretMap.Kp));
+  //   turretPID.setI(SmartDashboard.getNumber("I", RobotMap.TurretMap.Ki));
+  //   turretPID.setD(SmartDashboard.getNumber("D", 0));
+  //   turretPID.setIZone(SmartDashboard.getNumber("IZONE", RobotMap.TurretMap.KiZone));
+  // }
 
-
-  public double getEncoder() {
-    return turretEncoder.getPosition()*RobotMap.TurretMap.turretGearing;
+  public void setTurretDegrees(double degrees) {
+    turretPID.setReference(((degrees) * (RobotMap.TurretMap.turretDegreeToRotations)), ControlType.kPosition);
   }
 
+  /**
+   * Disables turret PID for manual control
+   */
+  public void disableTurretPID () {
+    turretPID.setP(0);
+    turretPID.setI(0);
+    turretPID.setD(0);
+  }
+
+  public void setCoast() {
+    turretMotor.setIdleMode(IdleMode.kCoast);
+  }
+
+  public void setBrake() {
+    turretMotor.setIdleMode(IdleMode.kBrake);
+  }
+
+  /**
+   * Enables turret PID for automatic control
+   */
+  public void enableTurretPID () {
+    turretPID.setP(RobotMap.TurretMap.Kp);
+    turretPID.setI(RobotMap.TurretMap.Ki);
+    turretPID.setIZone(RobotMap.TurretMap.KiZone);
+  }
+
+  private double returnError(){
+    return (getDegrees() - startDegrees);
+  }
+ 
   public void rotateTurret(double power) {
-    //limits how much the turret can turn when set to a power
-    if (Math.abs(getEncoder()-startPosition) < RobotMap.TurretMap.turnLimit) {
-      turretMotor.set(power);
-    }  
-    //When the power is set to reverse, it should allow the motor to go in the opposite direction, going below the limit
-    else if (-Math.signum(getEncoder()-startPosition) == Math.signum(power) && Math.abs(getEncoder()-startPosition) > RobotMap.TurretMap.turnLimit) {
-      turretMotor.set(power);
-    }
-    //Sets up the condition to be used when the turret wants to return home
-    else if (Math.abs(getEncoder()-startPosition) > RobotMap.TurretMap.turnLimit) {
-      limitReached = true;
-    }
-    else {
-      turretMotor.set(0);
-    }
+    turretMotor.set(power);
+  }
+  /**
+   * Returns rotations of NEO 550 (NOT TURRET)
+   * @return
+   */
+  public double getRotations() {
+    return turretMotor.getEncoder(EncoderType.kHallSensor, 42).getPosition();
+  }
+  /**
+   * Returns current heading of turret based on zeroing
+   * @return
+   */
+  public double getDegrees() {
+    return getRotations() / RobotMap.TurretMap.turretDegreeToRotations;
+  }
+
+  public int getEncoderCountsPerRevolution(){
+    return turretMotor.getEncoder().getCountsPerRevolution();
+  }
+
+  public CANSparkMax getMotor() {
+    return turretMotor;
   }
 
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Current Position", getEncoder());
-    SmartDashboard.putNumber("Start position", startPosition);
+    SmartDashboard.putNumber("Turret Degree", getDegrees());
     SmartDashboard.updateValues();
   }
-
 } 
