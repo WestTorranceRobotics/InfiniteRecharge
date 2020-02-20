67c49c6af07e0852bebcfef77f67c1aa17171780
Randall-Scharpf <43557842+Randall-Scharpf@users.noreply.github.com>
2020-02-20 15:20:27 -0800

    fixed rotation code

--- src/main/java/frc5124/robot2020/commands/loader/SeeBallRunBelt.java
+++ src/main/java/frc5124/robot2020/commands/loader/SeeBallRunBelt.java
@@ -7,14 +7,22 @@
 
 package frc5124.robot2020.commands.loader;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc5124.robot2020.subsystems.Loader;
 
 public class SeeBallRunBelt extends CommandBase {
-  double thicc = 0;
-  double no = 0;
-  boolean sawball = false;
+  double loaderdelay = 0;
+  private boolean sawball = false;
+  private boolean runbelt = false;
+  private boolean startedWaiting = false;
   private Loader m_Loader;
+  double intialPosition;
+  double currentPosition;
+
+  private long startWaiting;
+  private long waitTime = 500;
+  private long middleWaiting;
 
   public SeeBallRunBelt(Loader subsystem) {
     m_Loader = subsystem;
@@ -28,27 +36,71 @@ public class SeeBallRunBelt extends CommandBase {
   public void initialize() {
   }
 
+
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (m_Loader.seeBall()) {
-      if (sawball == false) {
-        m_Loader.runBelt();
-        sawball = true;
-      }
+    if(m_Loader.seeBall()){
+      sawball = true;
     }
-    if (sawball) {
-      if (no < m_Loader.returnRotations()) {
-        thicc++;
-        no++;
-      }
-      if (thicc >= 26) {
+    if(sawball == true && runbelt == false){
+      intialPosition = m_Loader.returnRotations();
+      m_Loader.runBelt();
+      sawball = false;
+      runbelt = true;
+    }
+    if(runbelt){
+      currentPosition = m_Loader.returnRotations(); 
+      if(currentPosition - intialPosition > 26){
         m_Loader.stopBelt();
-        thicc = 0;
-        sawball = false;
+        runbelt = false;
       }
     }
-    // 1000 is just a placeholder, after we test for optimal time we'll replace it
+
+    SmartDashboard.putNumber("Current position", currentPosition);
+    SmartDashboard.putNumber("Intial Position", intialPosition);
+    SmartDashboard.putBoolean("Sawball", sawball);
+    SmartDashboard.putBoolean("runbelt", runbelt);
+    SmartDashboard.updateValues();
+
+    // if (startedWaiting == false){
+    //   startWaiting = System.currentTimeMillis();
+    //   startedWaiting = true;
+    // }
+    // middleWaiting = System.currentTimeMillis();
+    // if (middleWaiting-startWaiting >= waitTime){
+    //   m_Loader.runBelt();
+    //   runbelt = true;
+    //   sawball = false;
+      
+    // }
+
+
+
+    // if (m_Loader.seeBall()) {
+    //   if (sawball == false) {
+    //     sawball = true;
+    //   }
+    // }
+    // if (loaderdelay == 0 && sawball && runbelt == false) {
+    //   m_Loader.runBelt();
+    //   runbelt = true;
+    // } else {
+    //   loaderdelay--;
+    // }
+    // if (runbelt) {
+    //   if (no < m_Loader.returnRotations()) {
+    //     thicc++;
+    //     no++;
+    //   }
+    //   if (thicc >= 26) {
+    //     m_Loader.stopBelt();
+    //     thicc = 0;
+    //     sawball = false;
+    //     runbelt = false;
+    //   }
+    // }
+    // 26 is just a placeholder, after we test for optimal rotations we'll replace it
   }
 
   // Called once the command ends or is interrupted.
