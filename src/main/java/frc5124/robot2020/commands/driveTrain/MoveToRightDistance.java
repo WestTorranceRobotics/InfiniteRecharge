c8884247bf54fd8875659a197f0d0b5eb18d689c
IanWearsHat <ianbbdai@gmail.com>
2021-05-18 16:24:37 -0700

    u

---
+++ src/main/java/frc5124/robot2020/commands/driveTrain/MoveToRightDistance.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc5124.robot2020.commands.driveTrain;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class MoveToRightDistance extends CommandBase {
+  private DriveTrain subsystem;
+  /** Creates a new MoveToRightDistance. */
+  public MoveToRightDistance(DriveTrain subsystem) {
+    this.subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+    double d = (98.25 - 21) / Math.tan(Math.toRadians(20.5 + ty));
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
